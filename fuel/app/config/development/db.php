cb0afa33736760c8eebbc0154966ad69436db376
Daniel Sposito <dsposito@static.com>
2013-07-12 12:50:20 -0500

    Fixes default developement db config file.

---
+++ fuel/app/config/development/db.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * The development database settings. These get merged with the global settings.
+ */
+
+return array(
+	'default' => array(
+		'connection'  => array(
+			'dsn'        => 'mysql:host=localhost;dbname=ereceivables',
+			'username'   => 'root',
+			'password'   => '',
+		),
+	),
+);
