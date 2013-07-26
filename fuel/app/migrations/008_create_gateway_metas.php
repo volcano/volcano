1713f5d0c8cabef71a4811f4cde1c0e4111528cc
Daniel Sposito <dsposito@static.com>
2013-07-26 16:53:28 -0500

    Cleaned up spacing for gateway migrations.

--- fuel/app/migrations/008_create_gateway_metas.php
+++ fuel/app/migrations/008_create_gateway_metas.php
@@ -12,12 +12,12 @@ class Create_Gateway_Metas
 	public function up()
 	{
 		\DBUtil::create_table('gateway_metas', array(
-			'id'			=> array('type' => 'int', 'constraint' => 11, 'auto_increment' => true),
-			'gateway_id'	=> array('type' => 'int', 'constraint' => 11,),
-			'name'			=> array('type' => 'varchar', 'constraint' => 255),
-			'value'			=> array('type' => 'text'),
-			'created_at'	=> array('type' => 'datetime'),
-			'updated_at'	=> array('type' => 'datetime'),
+			'id'         => array('type' => 'int', 'constraint' => 11, 'auto_increment' => true),
+			'gateway_id' => array('type' => 'int', 'constraint' => 11,),
+			'name'       => array('type' => 'varchar', 'constraint' => 255),
+			'value'      => array('type' => 'text'),
+			'created_at' => array('type' => 'datetime'),
+			'updated_at' => array('type' => 'datetime'),
 		), array('id'));
 		
 		\DBUtil::create_index('gateway_metas', 'gateway_id', 'gateway_id');
