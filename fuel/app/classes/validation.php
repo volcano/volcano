2ea7496ce9608e801b1da04616cdf4ae2167b662
Daniel Sposito <dsposito@static.com>
2013-07-30 15:56:30 -0500

    Added credit card luhn validation rule, contact validation rule and added nested param support to errors.

--- fuel/app/classes/validation.php
+++ fuel/app/classes/validation.php
@@ -14,6 +14,182 @@ class Validation extends \Fuel\Core\Validation
 	 */
 	public function errors()
 	{
-		return implode(', ', array_keys($this->errors));
+		$errors = array();
+		foreach ($this->errors as $key => $error) {
+			if (($message = $this->get_message($key)) && is_array($message)) {
+				foreach ($message as $msg) {
+					$errors[] = "$key.$msg";
+				}
+				
+			} else {
+				$errors[] = $key;
+			}
+		}
+		
+		return implode(', ', $errors);
+	}
+	
+	public function _validation_contact($data, $type = 'create')
+	{
+		if (!in_array($type, array('create', 'update'))) {
+			return false;
+		}
+		
+		$validator = \Validation_Contact::$type();
+		if (!$validator->run($data)) {
+			$this->set_message('contact', array_keys($validator->errors));
+			return false;
+		}
+		
+		return true;
+	}
+	
+	/**
+	 * Validates a credit card number.
+	 *
+	 * @param string $val Credit card number to validate.
+	 * @param string $type Credit card type (Visa, Discover, etc).
+	 *
+	 * @return bool
+	 */
+	public function _validation_creditcard($val, $type = null)
+	{
+		if ($this->_empty($val)) {
+			return false;
+		}
+		
+		// Remove all non-digit characters from the string.
+		if (($val = preg_replace('/\D+/', '', $val)) === '') {
+			return false;
+		}
+		
+		if ($type == null) {
+			$type = $this->creditcard_type($val, 'key');
+		} elseif (is_array($type)) {
+			foreach ($type as $t) {
+				// Test each type for validity.
+				if ($this->_validation_creditcard($val, $t)) {
+					return true;
+				}
+			}
+			
+			return false;
+		}
+		
+		Config::load('creditcards', true);
+		
+		$cards = Config::get('creditcards');
+		
+		$type = strtolower($type);
+		
+		if (!isset($cards[$type])) {
+			return false;
+		}
+		
+		$length = strlen($val);
+		
+		// Validate card length by the card type.
+		if (!in_array($length, preg_split('/\D+/', $cards[$type]['length']))) {
+			return false;
+		}
+		
+		// Check card number prefix.
+		if (!preg_match('/^'.$cards[$type]['prefix'].'/', $val)) {
+			return false;
+		}
+		
+		// No Luhn check required.
+		if ($cards[$type]['luhn'] == false) {
+			return true;
+		}
+		
+		return $this->luhn_check($val);
+	}
+	
+	/**
+	 * Performs the Luhn formula against a numeric value.
+	 * 
+	 * @see http://en.wikipedia.org/wiki/Luhn_algorithm
+	 *
+	 * @param string $val Number to validate.
+	 * 
+	 * @return bool
+	 */
+	protected function luhn_check($val)
+	{
+		// Force the value to be a string as this method uses string functions.
+		// Converting to an integer may pass PHP_INT_MAX and result in an error!
+		$val = (string) $val;
+		
+		// Luhn can only be used on numbers!
+		if (!ctype_digit($val)) {
+			return false;
+		}
+		
+		$length = strlen($val);
+		
+		$checksum = 0;
+		
+		for ($i = $length - 1; $i >= 0; $i -= 2) {
+			// Add up every 2nd digit, starting from the right.
+			$checksum += substr($val, $i, 1);
+		}
+		
+		for ($i = $length - 2; $i >= 0; $i -= 2) {
+			// Add up every 2nd digit doubled, starting from the right.
+			$double = substr($val, $i, 1) * 2;
+			
+			// Subtract 9 from the double where value is greater than 10.
+			$checksum += ($double >= 10) ? ($double - 9) : $double;
+		}
+		
+		// If the checksum is a multiple of 10, the number is valid.
+		return ($checksum % 10 === 0);
+	}
+	
+	
+	/**
+	 * Gets a credit card type.
+	 *
+	 * @param string $val Credit card type.
+	 * @param string $return The type of credit card data to return.
+	 *
+	 * @return string|bool
+	 */
+	protected function creditcard_type($val, $return = 'name')
+	{
+		// Remove all non-digit characters from the number
+		if (($val = preg_replace('/\D+/', '', $val)) === '') {
+			return false;
+		}
+		
+		if ($this->_empty($val)) {
+			return 'unknown';
+		}
+		
+		Config::load('creditcards', true);
+		
+		$cards = Config::get('creditcards');
+		
+		foreach ($cards as $key => $card) {
+			if (empty($card['prefix'])) {
+				continue;
+			}
+			
+			$prefixes = explode(',', $card['prefix']);
+			$lengths = explode(',', $card['length']);
+			
+			foreach ($prefixes as $prefix) {
+				if (preg_match('/^' . $prefix . '/', $val)) {
+					foreach ($lengths as $length) {
+						if (strlen($val) == $length) {
+							return ($return == 'name') ? $card['name'] : $key;
+						}
+					}
+				}
+			}
+		}
+		
+		return 'unknown';
 	}
 }
