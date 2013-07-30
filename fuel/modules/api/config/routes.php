cb10c948875ee9a8c6bc44a02fc3ebffa2f316f9
Daniel Sposito <dsposito@static.com>
2013-07-30 16:40:56 -0500

    Simplied routes for nested api controllers.

--- fuel/modules/api/config/routes.php
+++ fuel/modules/api/config/routes.php
@@ -2,10 +2,11 @@
 
 return array(
 	// Allows the index action to not have to be in the URL.
-	'api/(:segment)/(:num)'                        => 'api/$1/index/$2',
+	'api/(:segment)/(:num)'                                     => 'api/$1/index/$2',
 	
-	'api/products/(:num)/options'                  => 'api/products/options/index/$1',
-	'api/products/(:num)/options/(:num)'           => 'api/products/options/index/$1/$2',
-	'api/products/:num/options/(:num)/fees'        => 'api/products/options/fees/index/$1',
-	'api/products/:num/options/(:num)/fees/(:num)' => 'api/products/options/fees/index/$1/$2',
+	'api/(:segment)/(:num)/(:segment)'                          => 'api/$1/$3/index/$2',
+	'api/(:segment)/(:num)/(:segment)/(:num)'                   => 'api/$1/$3/index/$2/$4',
+	
+	'api/(:segment)/(:num)/(:segment)/(:num)/(:segment)'        => 'api/$1/$3/$5/index/$4',
+	'api/(:segment)/(:num)/(:segment)/(:num)/(:segment)/(:num)' => 'api/$1/$3/$5/index/$4/$6',
 );
