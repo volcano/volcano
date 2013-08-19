80e207fce31ec0d6a159b84f11144493dc4953ab
Daniel Sposito <dsposito@static.com>
2013-08-19 11:48:42 -0500

    Updated env db configs.

--- fuel/app/config/development/db.php
+++ fuel/app/config/development/db.php
@@ -6,7 +6,7 @@
 return array(
 	'default' => array(
 		'connection'  => array(
-			'dsn'        => 'mysql:host=localhost;dbname=ereceivables',
+			'dsn'        => 'mysql:host=localhost;dbname=volcano',
 			'username'   => 'root',
 			'password'   => '',
 		),
--- fuel/app/config/staging/db.php
+++ fuel/app/config/staging/db.php
@@ -1,14 +1,27 @@
 <?php
-/**
- * The staging database settings. These get merged with the global settings.
- */
+
+$db_config = array();
+$db_config_file = '/var/www/volcano.static.com/config/db.php';
+if (file_exists($db_config_file)) {
+	$tmp_db_config = include $db_config_file;
+	if ($tmp_db_config && is_array($tmp_db_config)) {
+		$db_config = $tmp_db_config;
+	}
+}
+
+$host = Arr::get($db_config, 'default.connection.host');
+$name = Arr::get($db_config, 'default.connection.name');
+$user = Arr::get($db_config, 'default.connection.user');
+$pass = Arr::get($db_config, 'default.connection.pass');
 
 return array(
 	'default' => array(
 		'connection'  => array(
-			'dsn'        => 'mysql:host=localhost;dbname=ereceivables',
-			'username'   => 'fuel_app',
-			'password'   => 'super_secret_password',
+			'dsn'      => "mysql:host=$host;dbname=$name",
+			'username' => $user,
+			'password' => $pass,
 		),
+		'enable_cache' => true,
+		'profiling'    => false,
 	),
 );
