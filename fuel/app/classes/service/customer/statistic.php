e02613a2db71868e928fcf85a3dca1da3e8823e9
Daniel Sposito <dsposito@static.com>
2013-08-23 18:03:07 -0500

    Added a statistics task (to be run as a cron) and related customer statistic service.

---
+++ fuel/app/classes/service/customer/statistic.php
@@ -0,0 +1,166 @@
+<?php
+
+/**
+ * Customer statistic service.
+ *
+ * @author Daniel Sposito <dsposito@static.com>
+ */
+class Service_Customer_Statistic extends Service
+{
+	/**
+	 * Calculates the total number of customers created in a given date range.
+	 *
+	 * @param Date $begin Begin date.
+	 * @param Date $end   End date.
+	 *
+	 * @return array
+	 */
+	public static function created(Date $begin, Date $end)
+	{
+		// Retrieve total number of customers created during begin/end date range.
+		$query = DB::select('seller_id', array(DB::expr('COUNT(1)'), 'total'))
+			->from('customers')
+			->where('status', 'active')
+			->where('created_at', '>=', $begin->format('mysql'))
+			->where('created_at', '<=', $end->format('mysql'))
+			->group_by('seller_id');
+		
+		$results = $query->execute();
+		
+		return $results->as_array();
+	}
+	
+	/**
+	 * Calculates the total number of customers deleted in a given date range.
+	 *
+	 * @param Date $begin Begin date.
+	 * @param Date $end   End date.
+	 *
+	 * @return array
+	 */
+	public static function deleted(Date $begin, Date $end)
+	{
+		// Retrieve total number of customers deleted during begin/end date range.
+		$query = DB::select('seller_id', array(DB::expr('COUNT(1)'), 'total'))
+			->from('customers')
+			->where('status', 'deleted')
+			->where('updated_at', '>=', $begin->format('mysql'))
+			->where('updated_at', '<=', $end->format('mysql'))
+			->group_by('seller_id');
+		
+		$results = $query->execute();
+		
+		return $results->as_array();
+	}
+	
+	/**
+	 * Calculates the total number of customers that bought a product in a given date range.
+	 *
+	 * @param Date $begin Begin date.
+	 * @param Date $end   End date.
+	 *
+	 * @return array
+	 */
+	public static function subscribed(Date $begin, Date $end)
+	{
+		// Retrieve total number of subscriptions created during begin/end date range.
+		$query = DB::select('c.seller_id', array(DB::expr('COUNT(DISTINCT customer_id)'), 'total'))
+			->from(array('customer_product_options', 'cpo'))
+			->join(array('customers', 'c'))->on('cpo.customer_id', '=', 'c.id')
+			->where('cpo.status', 'active')
+			->where('cpo.created_at', ' >=', $begin->format('mysql'))
+			->where('cpo.created_at', ' <=', $end->format('mysql'))
+			->group_by('c.seller_id');
+		
+		$results = $query->execute();
+		
+		return $results->as_array();
+	}
+	
+	/**
+	 * Calculates the total number of customers that canceled a product in a given date range.
+	 *
+	 * @param Date $begin Begin date.
+	 * @param Date $end   End date.
+	 *
+	 * @return array
+	 */
+	public static function unsubscribed(Date $begin, Date $end)
+	{
+		// Retrieve total number of subscriptions canceled during begin/end date range.
+		$query = DB::select('c.seller_id', array(DB::expr('COUNT(DISTINCT customer_id)'), 'total'))
+			->from(array('customer_product_options', 'cpo'))
+			->join(array('customers', 'c'))->on('cpo.customer_id', '=', 'c.id')
+			->where('cpo.status', 'canceled')
+			->where('cpo.updated_at', ' >=', $begin->format('mysql'))
+			->where('cpo.updated_at', ' <=', $end->format('mysql'))
+			->group_by('c.seller_id');
+		
+		$results = $query->execute();
+		
+		return $results->as_array();
+	}
+	
+	/**
+	 * Calculates the total number of customers created prior to a given date.
+	 *
+	 * @param Date $date End date.
+	 *
+	 * @return array
+	 */
+	public static function total(Date $date)
+	{
+		// Retrieve total number of customers created prior to the specified date.
+		$query = DB::select('seller_id', array(DB::expr('COUNT(1)'), 'total'))
+			->from('customers')
+			->where('updated_at', '<=', $date->format('mysql'))
+			->group_by('seller_id');
+		
+		$results = $query->execute();
+		
+		return $results->as_array();
+	}
+	
+	/**
+	 * Calculates the total number of active customers prior to a given date.
+	 *
+	 * @param Date $date End date.
+	 *
+	 * @return array
+	 */
+	public static function total_active(Date $date)
+	{
+		// Retrieve total number of customers created prior to the specified date.
+		$query = DB::select('seller_id', array(DB::expr('COUNT(1)'), 'total'))
+			->from('customers')
+			->where('status', 'active')
+			->where('updated_at', '<=', $date->format('mysql'))
+			->group_by('seller_id');
+		
+		$results = $query->execute();
+		
+		return $results->as_array();
+	}
+	
+	/**
+	 * Calculates the total number of customers that bought a product prior to a given date.
+	 *
+	 * @param Date $date End date.
+	 *
+	 * @return array
+	 */
+	public static function total_subscribed(Date $date)
+	{
+		// Retrieve total number of subscriptions created during begin/end date.
+		$query = DB::select('c.seller_id', array(DB::expr('COUNT(DISTINCT customer_id)'), 'total'))
+			->from(array('customer_product_options', 'cpo'))
+			->join(array('customers', 'c'))->on('cpo.customer_id', '=', 'c.id')
+			->where('cpo.status', 'active')
+			->where('cpo.updated_at', ' <=', $date->format('mysql'))
+			->group_by('c.seller_id');
+		
+		$results = $query->execute();
+		
+		return $results->as_array();
+	}
+}
---
+++ fuel/app/tasks/statistics.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace Fuel\Tasks;
+
+/**
+ * Statistics task.
+ *
+ * @author Daniel Sposito <dsposito@static.com>
+ */
+class Statistics
+{
+	/**
+	 * Processes all statistic calculations.
+	 *
+	 * @return void
+	 */
+	public static function run()
+	{
+		self::customer();
+		
+		\Cli::write('All Statistical Calculations Complete', 'green');
+	}
+	
+	/**
+	 * Processes customer statistics.
+	 *
+	 * @return void
+	 */
+	public static function customer()
+	{
+		if (!$task = \Service_Statistic_Task::begin('customer')) {
+			return false;
+		}
+		
+		$data = array();
+		
+		$date_ranges = \Service_Statistic_Task::date_ranges('customer');
+		foreach ($date_ranges as $range) {
+			$begin = \Date::create_from_string($range['begin'], 'mysql');
+			$end   = \Date::create_from_string($range['end'], 'mysql');
+			$date  = $begin->format('mysql_date');
+			
+			$data[$date] = array();
+			
+			$created = \Service_Customer_Statistic::created($begin, $end);
+			foreach ($created as $result) {
+				$data[$date][] = array(
+					'seller_id' => $result['seller_id'],
+					'name'      => 'created',
+					'value'     => $result['total'],
+				);
+			}
+			
+			$deleted = \Service_Customer_Statistic::deleted($begin, $end);
+			foreach ($deleted as $result) {
+				$data[$date][] = array(
+					'seller_id' => $result['seller_id'],
+					'name'      => 'deleted',
+					'value'     => $result['total'],
+				);
+			}
+			
+			$subscribed = \Service_Customer_Statistic::subscribed($begin, $end);
+			foreach ($subscribed as $result) {
+				$data[$date][] = array(
+					'seller_id' => $result['seller_id'],
+					'name'      => 'subscribed',
+					'value'     => $result['total'],
+				);
+			}
+			
+			$unsubscribed = \Service_Customer_Statistic::unsubscribed($begin, $end);
+			foreach ($unsubscribed as $result) {
+				$data[$date][] = array(
+					'seller_id' => $result['seller_id'],
+					'name'      => 'unsubscribed',
+					'value'     => $result['total'],
+				);
+			}
+			
+			$total = \Service_Customer_Statistic::total($end);
+			foreach ($total as $result) {
+				$data[$date][] = array(
+					'seller_id' => $result['seller_id'],
+					'name'      => 'total',
+					'value'     => $result['total'],
+				);
+			}
+			
+			$total_active = \Service_Customer_Statistic::total_active($end);
+			foreach ($total_active as $result) {
+				$data[$date][] = array(
+					'seller_id' => $result['seller_id'],
+					'name'      => 'total_active',
+					'value'     => $result['total'],
+				);
+			}
+			
+			$total_subscribed = \Service_Customer_Statistic::total_subscribed($end);
+			foreach ($total_subscribed as $result) {
+				$data[$date][] = array(
+					'seller_id' => $result['seller_id'],
+					'name'      => 'total_subscribed',
+					'value'     => $result['total'],
+				);
+			}
+		}
+		
+		// Save the queried results as statistics.
+		foreach ($data as $date => $results) {
+			$date = \Date::create_from_string($date, 'mysql_date');
+			
+			foreach ($results as $result) {
+				$seller = \Service_Seller::find_one($result['seller_id']);
+				
+				if (!\Service_Statistic::create($seller, 'customer', $date, $result['name'], $result['value'])) {
+					\Service_Statistic_Task::end($task, 'failed', "Error creating customer.{$result['name']} statistic for seller {$seller->name}.");
+					return;
+				}
+			}
+		}
+		
+		\Service_Statistic_Task::end($task);
+		
+		\Cli::write('Customer Statistical Calculations Complete', 'green');
+	}
+}
