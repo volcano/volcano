135065a53700bc0ff07f31c5236ee3007f4ade58
Daniel Sposito <dsposito@static.com>
2013-08-28 15:57:07 -0500

    Added API for retrieving and creating customer orders.

---
+++ fuel/app/classes/validation/customer/order.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * Validation customer order class.
+ *
+ * @author Daniel Sposito <dsposito@static.com>
+ */
+class Validation_Customer_Order
+{
+	/**
+	 * Creates a new validation instance for order create.
+	 *
+	 * @param Model_Customer $customer The ordering customer.
+	 *
+	 * @return Validation
+	 */
+	public static function create(Model_Customer $customer)
+	{
+		$validator = Validation::forge('order');
+		
+		$validator->add('products', 'Products')->add_rule('required')->add_rule(array('invalid_products' => function ($products) use ($customer) {
+			$ids = array_keys($products);
+			
+			foreach ($ids as $id) {
+				if (!is_numeric($id)) {
+					return false;
+				}
+				
+				$product_option = Service_Product_Option::find_one($id);
+				if (!$product_option || $product_option->product->seller != $customer->seller) {
+					return false;
+				}
+			}
+			
+			return true;
+		}));
+		
+		$validator->add('paymentmethod', 'Paymentmethod')->add_rule(array('invalid_paymentmethod' => function ($paymentmethod) use ($customer) {
+			if (!$paymentmethod) {
+				return true;
+			}
+			
+			$paymentmethod = Service_Customer_Paymentmethod::find_one($paymentmethod);
+			if (!$paymentmethod || $paymentmethod->customer != $customer) {
+				return false;
+			}
+			
+			return true;
+		}));
+		
+		return $validator;
+	}
+}
---
+++ fuel/modules/api/classes/controller/customers/orders.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Api;
+
+/**
+ * Customer orders controller.
+ * 
+ * @author Daniel Sposito <dsposito@static.com>
+ */
+class Controller_Customers_Orders extends Controller
+{
+	/**
+	 * Gets one or more orders.
+	 *
+	 * @param int $customer_id Customer ID.
+	 * @param int $id          Payment method ID.
+	 *
+	 * @return void
+	 */
+	public function get_index($customer_id = null, $id = null)
+	{
+		$customer = $this->get_customer($customer_id);
+		
+		if (!$id) {
+			$orders = \Service_Customer_Order::find(array(
+				'customer' => $customer,
+			));
+		} else {
+			$orders = \Service_Customer_Order::find_one($id);
+			if (!$orders || $orders->customer != $customer || $order->customer->seller != \Seller::active()) {
+				throw new HttpNotFoundException;
+			}
+		}
+		
+		$this->response($orders);
+	}
+	
+	/**
+	 * Creates an order.
+	 *
+	 * @param int $customer_id Customer ID.
+	 *
+	 * @return void
+	 */
+	public function post_index($customer_id = null)
+	{
+		$customer = $this->get_customer($customer_id);
+		
+		$validator = \Validation_Customer_Order::create($customer);
+		if (!$validator->run()) {
+			throw new HttpBadRequestException($validator->errors());
+		}
+		
+		$data = $validator->validated();
+		
+		$order = \Service_Customer_Order::create($customer, $data['products'], $data['paymentmethod']);
+		if (!$order) {
+			throw new HttpServerErrorException;
+		}
+		
+		$this->response($order);
+	}
+	
+	/**
+	 * Attempts to get a customer from a given ID.
+	 *
+	 * @param int $id Customer ID.
+	 *
+	 * @return \Model_Customer
+	 */
+	protected function get_customer($id)
+	{
+		if (!$id) {
+			throw new HttpNotFoundException;
+		}
+		
+		$customer = \Service_Customer::find_one($id);
+		if (!$customer || $customer->seller != \Seller::active()) {
+			throw new HttpNotFoundException;
+		}
+		
+		return $customer;
+	}
+}
