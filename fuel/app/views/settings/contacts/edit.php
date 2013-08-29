5adf1184ba84e93ee9a40aa2c9b8e62720f175ab
Daniel Sposito <dsposito@static.com>
2013-08-29 13:58:45 -0500

    Updated breadcrumbs.

--- fuel/app/views/settings/contacts/edit.php
+++ fuel/app/views/settings/contacts/edit.php
@@ -1,7 +1,8 @@
 <?php
 $layout->title = 'Edit';
 $layout->subtitle = $contact->company_name;
-$layout->breadcrumbs['Contacts'] = 'settings';
+$layout->breadcrumbs['Settings'] = 'settings';
+$layout->breadcrumbs['Contacts'] = 'settings/contacts';
 $layout->breadcrumbs['Edit: ' . $contact->company_name] = '';
 
 echo render('settings/contacts/form', array(
