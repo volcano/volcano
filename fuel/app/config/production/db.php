6f4b16ebd0d0fd42de2f10d8a44e419e438cb7b6
Daniel Sposito <daniel.g.sposito@gmail.com>
2014-04-01 21:16:37 -0500

    Remove non-core elements from db configs.

--- fuel/app/config/production/db.php
+++ fuel/app/config/production/db.php
@@ -1,25 +1,11 @@
 <?php
 
-$db_config = array();
-$db_config_file = '/var/www/volcano.catalog.com/config/db.php';
-if (file_exists($db_config_file)) {
-	$tmp_db_config = include $db_config_file;
-	if ($tmp_db_config && is_array($tmp_db_config)) {
-		$db_config = $tmp_db_config;
-	}
-}
-
-$host = Arr::get($db_config, 'default.connection.host');
-$name = Arr::get($db_config, 'default.connection.name');
-$user = Arr::get($db_config, 'default.connection.user');
-$pass = Arr::get($db_config, 'default.connection.pass');
-
 return array(
 	'default' => array(
 		'connection'  => array(
-			'dsn'      => "mysql:host=$host;dbname=$name",
-			'username' => $user,
-			'password' => $pass,
+			'dsn'      => "mysql:host=localhost;dbname=volcano",
+			'username' => '[USERNAME]',
+			'password' => '[PASSWORD]',
 		),
 		'enable_cache' => true,
 		'profiling'    => false,
