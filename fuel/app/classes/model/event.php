a71013f5a5b323be11c52b592ee38501cc503f67
Daniel Sposito <daniel.g.sposito@gmail.com>
2014-04-03 20:39:45 -0500

    Add a magic toString() to the event model.

--- fuel/app/classes/model/event.php
+++ fuel/app/classes/model/event.php
@@ -9,4 +9,14 @@ class Model_Event extends Model
 		'id',
 		'name',
 	);
+	
+	/**
+	 * Returns this model's string representation.
+	 *
+	 * @return string
+	 */
+	public function __toString()
+	{
+		return Inflector::titleize($this->name, '.');
+	}
 }
