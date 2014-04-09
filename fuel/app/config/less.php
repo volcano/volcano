46840d71fb3b3193b35767edac1ea6025d050e2b
Derek Myers <arcticpro@gmail.com>
2014-04-08 21:45:22 -0500

    Removes old unsupported less package configs

--- fuel/app/config/less.php
+++ fuel/app/config/less.php
@@ -20,19 +20,4 @@ return array(
 	 * Default: Config::get('asset.paths.0').Config::get('asset.css_dir'),
 	 */
 	'output_dir' => Casset::get_cache_path(),
-	
-	/**
-	 * Whether or not to keep the directory that the file is in or just
-	 * store in the main output_dir.
-	 *
-	 * Default: true
-	 */
-	'keep_dir' => false,
-	
-	/**
-	 * Whether or not to to hash the filename of the less compiled file.
-	 *
-	 * Default: false
-	 */
-	'hash_filename' => true,
 );
