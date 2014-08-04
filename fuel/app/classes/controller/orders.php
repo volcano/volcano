a8c597f6cee6d14f7d6473fd3e627bffb2dcbd6e
Alexander Ward <daginus@gmail.com>
2014-08-04 14:35:58 +0000

    Tabs, not spaces

--- fuel/app/classes/controller/orders.php
+++ fuel/app/classes/controller/orders.php
@@ -13,20 +13,20 @@
         );
         
         $pagination = Pagination::forge('customer_pagination', array(
-			'total_items' => Service_Customer_Order::count($args),
-		));
-		
-		$orders = Service_Customer_Order::find(array_merge($args, array(
-			'offset' => $pagination->offset,
-			'limit'  => $pagination->per_page,
-			'sort'  => array(
-			    'field' => 'updated_at',
-			    'dir'   => 'desc',
-			 ),
-		)));
-		
-		$this->view->orders = $orders;
-		$this->view->pagination = $pagination;
+            'total_items' => Service_Customer_Order::count($args),
+        ));
+        
+        $orders = Service_Customer_Order::find(array_merge($args, array(
+            'offset' => $pagination->offset,
+            'limit'  => $pagination->per_page,
+            'sort'  => array(
+                'field' => 'updated_at',
+                'dir'   => 'desc',
+             ),
+        )));
+        
+        $this->view->orders = $orders;
+        $this->view->pagination = $pagination;
     }
  }
  
