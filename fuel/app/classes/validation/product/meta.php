add36ad811284343513cccd05cee58c76c4c0217
Daniel Sposito <daniel.g.sposito@gmail.com>
2014-11-13 00:11:39 -0600

    Adds product meta validator.

---
+++ fuel/app/classes/validation/product/meta.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * Validation product meta class.
+ */
+class Validation_Product_Meta
+{
+	/**
+	 * Creates a new validation instance for product meta create.
+	 *
+	 * @return Validation
+	 */
+	public static function create()
+	{
+		$validator = Validation::forge('product_meta');
+		
+		$validator->add('name', 'Name')->add_rule('trim')->add_rule('required');
+		$validator->add('option', 'Option')->add_rule('required');
+		
+		return $validator;
+	}
+	
+	/**
+	 * Creates a new validation instance for product meta update.
+	 *
+	 * @return Validation
+	 */
+	public static function update()
+	{
+		$validator = Validation::forge('product_meta');
+		
+		$input = Input::param();
+		
+		if (array_key_exists('name', $input)) {
+			$validator->add('name', 'Name')->add_rule('trim')->add_rule('required');
+		}
+		
+		if (array_key_exists('option', $input)) {
+			$validator->add('option', 'Option')->add_rule('required');
+		}
+		
+		return $validator;
+	}
+}
