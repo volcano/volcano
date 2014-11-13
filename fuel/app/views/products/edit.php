46fc60bd168506cf928fb21a70589f411a1cb2e9
Daniel Sposito <daniel.g.sposito@gmail.com>
2014-11-13 00:01:51 -0600

    Updates edit product breadcrumbs and adds left nav.

--- fuel/app/views/products/edit.php
+++ fuel/app/views/products/edit.php
@@ -1,8 +1,10 @@
 <?php
-$layout->title = 'Edit';
+$layout->title = 'General';
 $layout->subtitle = $product->name;
+$layout->leftnav = render('products/leftnav', array('product' => $product));
 $layout->breadcrumbs['Product Lines'] = 'products';
-$layout->breadcrumbs['Edit: ' . $product->name] = '';
+$layout->breadcrumbs[$product->name] = $product->link('edit');
+$layout->breadcrumbs['General'] = '';
 
 echo render('products/form', array(
 	'product' => $product,
